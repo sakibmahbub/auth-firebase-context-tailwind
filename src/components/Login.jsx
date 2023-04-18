f1b60202c027c2f95a6b1a9ca6922e1e039e436e
sakibmahbub <arafatsakib48@gmail.com>
2023-04-18 15:50:32 +0600

    added register form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,10 +5,10 @@ const Login = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col ">
         <div className="text-center lg:text-left mb-5">
-          <h1 className="text-5xl font-bold text-center">Login now!</h1>
+          <h1 className="text-3xl font-bold text-center">Login now!</h1>
         </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-xl bg-base-100">
-          <div className="card-body">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -41,7 +41,7 @@ const Login = () => {
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const Register = () => {
-  return <div></div>;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col ">
+        <div className="text-center lg:text-left mb-5">
+          <h1 className="text-3xl font-bold text-center">Register now!</h1>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
