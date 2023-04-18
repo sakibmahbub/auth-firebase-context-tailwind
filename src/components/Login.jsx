b94ecc36e337dd8722fdc6bcf988783d087a0641
sakibmahbub <arafatsakib48@gmail.com>
2023-04-18 16:06:30 +0600

    added handleLogin and handleRegister

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col ">
@@ -8,7 +17,7 @@ const Login = () => {
           <h1 className="text-3xl font-bold text-center">Login now!</h1>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -33,9 +42,9 @@ const Login = () => {
                 required
               />
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
+                <Link to="/register" className="label-text-alt link link-hover">
+                  New to Auth Wizard?
+                </Link>
               </label>
             </div>
             <div className="form-control mt-6">
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handleRegister = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col ">
@@ -8,7 +17,19 @@ const Register = () => {
           <h1 className="text-3xl font-bold text-center">Register now!</h1>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your name"
+                className="input input-bordered"
+                required
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -16,7 +37,7 @@ const Register = () => {
               <input
                 type="email"
                 name="email"
-                placeholder="email"
+                placeholder="Your email"
                 className="input input-bordered"
                 required
               />
@@ -28,14 +49,14 @@ const Register = () => {
               <input
                 type="password"
                 name="password"
-                placeholder="password"
+                placeholder="Your password"
                 className="input input-bordered"
                 required
               />
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
+                <Link to="/login" className="label-text-alt link link-hover">
+                  Already have an account? Please login
+                </Link>
               </label>
             </div>
             <div className="form-control mt-6">
